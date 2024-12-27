e297f5a11d0a52fb2db108c66e56ff7279f96b44
omarse7a <omar.seha90@gmail.com>
2024-12-27 14:56:20 +0200

    fixed the digit_arr storage

--- converter.asm
+++ converter.asm
@@ -1,10 +1,11 @@
 .data
 	
-currentSystem: .word    
-number:        .asciiz
-newSystem:     .word  
+currentSystem: .word
+number:        .space 32
+newSystem:     .word
 newNumber:     .asciiz
-digit_arr:	.word
+digit_arr: .word
+
  
 introductionMessage: .asciiz "Hello, This is Numbering System Converter. Please enter the current system, number and new system: \n"
 currentSystemMsg: .asciiz "Enter The Current System: "
@@ -15,17 +16,6 @@ outputMsg: .asciiz "\nThe Output Number: "
 .text
 
 main:
-	la $a0, introductionMessage
-	jal printMessage
-
-	la $a0, currentSystemMsg
-	jal printMessage
-    
-	# read current system
-	li $v0, 5       
-	syscall
-	sw $v0, currentSystem
-
 	la $a0, currentNumberMsg
 	jal printMessage
     
@@ -34,16 +24,6 @@ main:
 	la $a0, number 	         # load address of (number)
 	li $a1, 32      		 # max length
 	syscall
-    
-	la $a0, newSystemMsg
-	jal printMessage
-    
-	# read new system
-	li $v0, 5           	
-	syscall
-	sw $v0, newSystem
-    
-	lw $t0, currentSystem
     	
 	jal otherToDecimal
     
@@ -58,51 +38,52 @@ printMessage:
 	jr $ra
 
 otherToDecimal:
-	la $t2, number	# number base address
-	la $t5, digit_arr
+	la $t1, number	# number string base address
+	li $t2, 0	# number offset (counter)
+	la $t3, digit_arr # array's base address
+	li $t4, 0	# array offset
 	
 	# get each digit's decimal value and saving it to an array
 	digit_loop:
-		lb $t3, 0($t2)	# load a char into register
-		beqz $t3, digit_loop_end	# exit the loop when char is '\0'
-
+		add $t5, $t1, $t2	# adding offset to the base address
+		lb $t6, 0($t5)		# loading the char base+offset
 		
-		bgt $t3, '9', convert_alpha
+		beq $t6, '\n', digit_loop_end	# string end condition
 		
-		sub $t4, $t3, '0'	# converting a digit char to integer
+		bgt $t6, '9', convert_alpha
+		
+		subi $t7, $t6, '0' 	
 		
 		append_arr:
-			sw $t4, ($t5)	# storing the digit into digit_arr
-			sll $t5, $t5, 2	# move to the next word address
-		
-		addi $t2, $t2, 1	# increment the address by 1 byte
-		j digit_loop
-		
-	digit_loop_end:	
-		la $t6, digit_arr
-		reverse_loop:
-			subi $t2, $t2, 1  # decrement the counter (initially equal to array size)
-			bltz $t2, end
-			sll $t7, $t2, 2		# offset from base address
-			add $t8, $t6, $t7 	# current index address
-			# print the value
-			lw $a0, ($t8)
-			li $v0, 1 # Syscall code for print integer
+			add $t8, $t3, $t4
+			sw $t7, 0($t8)
+			
+			# printing each integer
+			lw $a0, 0($t8)
+			li $v0, 1
 			syscall
-			# print a newline
-			lw $a0, '\n'
+		
+			li $a0, '\n'
 			li $v0, 11
 			syscall
 			
-			j reverse_loop
-		end:
-			jr $ra
-
+			addi $t4, $t4, 4	# add a word size to the offset
+		
+		
+		addi $t2, $t2, 1		# move to the next byte address
+		j digit_loop
+		
+	digit_loop_end:
+		jr $ra	
+		
 # utility	
 convert_alpha:
-	sub $t4, $t3, 'A'	# converting an alpha digit char to integer
-	addi $t4, $t4, 10
+	# converting an alpha digit char to decimal
+	subi $t7, $t6, 'A'
+	addi $t7, $t7, 10 
+	
 	j append_arr
+
 
 
 
